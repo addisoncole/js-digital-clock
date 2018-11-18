8fa1a080588e2034e2c9ab81f13cb11bbdedd912
Addison Cole <addison.cole85@gmail.com>
2018-11-17 21:24:21 -0800

    add 5 separate clocks for different time zones, they also display hex color, but the red value is boosted in hex to create a gradiation to differentiate more between local clock and intl clocks

--- index.js
+++ index.js
@@ -12,30 +12,18 @@ $(document).ready(function() {
       let now = new Date();
       // let month = now.getMonth();
       let time = now.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', second: '2-digit'});
-      let hour = now.getHours();
-      let minute = now.getMinutes();
-      let second = now.getSeconds();
-
-      //brightens background color for daytime
-      if (hour > 7 && hour < 18) {
-            hour += 75;
-            minute += 75;
-            second += 75;
-          }
-
-      // convert to hex
-      hour = hour.toString(16);
-      minute = minute.toString(16);
-      second = second.toString(16);
+      let hour = now.getHours().toString();
+      let minute = now.getMinutes().toString();
+      let second = now.getSeconds().toString();
 
       //appends 0 in front if hex number is single digit
-      if (hour.length === 1) {
+      if (hour.length < 2) {
         hour = '0' + hour;
       }
-      if (minute.length === 1) {
+      if (minute.length < 2) {
         minute = '0' + minute;
       }
-      if (second.length === 1) {
+      if (second.length < 2) {
         second = '0' + second;
       }
 
@@ -48,11 +36,69 @@ $(document).ready(function() {
   function shanghaiClock() {
     let now = new Date().toLocaleString("en-US", {hour: '2-digit', minute:'2-digit', second:'2-digit', timeZone: "Asia/Shanghai"});
 
+
+        let element = document.getElementById('shanghai-clock');
+        let color = now.replace(/\D/g, '');
+        if (color.length < 6){
+          color = ('0'+ color);
+        }
+        color = color.match(/.{1,2}/g);
+
+        let hour = parseInt(color[0]);
+            hour += 75;
+            hour = hour.toString();
+        let minute = color[1];
+        let second = color[2];
+
+
+        // //appends 0 in front if hex number is single digit
+        if (hour.length < 2) {
+          hour = '0' + hour;
+        }
+        if (minute.length < 2) {
+          minute = '0' + minute;
+        }
+        if (second.length < 2) {
+          second = '0' + second;
+        }
+
+        element.style.backgroundColor = `#${hour}${minute}${second}`;
+
+
     $('#shanghai-clock').text(`shanghai ${now}`);
   }
 
   function londonClock() {
     let now = new Date().toLocaleString("en-US", {hour: '2-digit', minute:'2-digit', second:'2-digit', timeZone: "Europe/London"});
+
+
+        let element = document.getElementById('london-clock');
+        let color = now.replace(/\D/g, '');
+        if (color.length < 6){
+          color = ('0'+ color);
+        }
+        color = color.match(/.{1,2}/g);
+
+        let hour = parseInt(color[0]);
+            hour += 55;
+            hour = hour.toString();
+        let minute = color[1];
+        let second = color[2];
+
+
+        // //appends 0 in front if hex number is single digit
+        if (hour.length < 2) {
+          hour = '0' + hour;
+        }
+        if (minute.length < 2) {
+          minute = '0' + minute;
+        }
+        if (second.length < 2) {
+          second = '0' + second;
+        }
+
+        element.style.backgroundColor = `#${hour}${minute}${second}`;
+
 
     $('#london-clock').text(`london ${now}`);
   }
@@ -60,17 +106,100 @@ $(document).ready(function() {
   function hawaiiClock() {
     let now = new Date().toLocaleString("en-US", {hour: '2-digit', minute:'2-digit', second:'2-digit', timeZone: "Pacific/Honolulu"});
 
+
+        let element = document.getElementById('hawaii-clock');
+        let color = now.replace(/\D/g, '');
+        if (color.length < 6){
+          color = ('0'+ color);
+        }
+        color = color.match(/.{1,2}/g);
+
+        let hour = parseInt(color[0]);
+            hour += 25;
+            hour = hour.toString();
+        let minute = color[1];
+        let second = color[2];
+
+
+        // //appends 0 in front if hex number is single digit
+        if (hour.length < 2) {
+          hour = '0' + hour;
+        }
+        if (minute.length < 2) {
+          minute = '0' + minute;
+        }
+        if (second.length < 2) {
+          second = '0' + second;
+        }
+
+        element.style.backgroundColor = `#${hour}${minute}${second}`;
+
+
     $('#hawaii-clock').text(`honolulu ${now}`);
   }
 
   function newYorkClock() {
     let now = new Date().toLocaleString("en-US", {hour: '2-digit', minute:'2-digit', second:'2-digit', timeZone: "America/New_York"});
 
+    let element = document.getElementById('newyork-clock');
+    let color = now.replace(/\D/g, '');
+    if (color.length < 6){
+      color = ('0'+ color);
+    }
+    color = color.match(/.{1,2}/g);
+
+    let hour = parseInt(color[0]);
+        hour += 40;
+        hour = hour.toString();
+    let minute = color[1];
+    let second = color[2];
+
+
+    // //appends 0 in front if hex number is single digit
+    if (hour.length < 2) {
+      hour = '0' + hour;
+    }
+    if (minute.length < 2) {
+      minute = '0' + minute;
+    }
+    if (second.length < 2) {
+      second = '0' + second;
+    }
+
+    element.style.backgroundColor = `#${hour}${minute}${second}`;
+
     $('#newyork-clock').text(`new york ${now}`);
   }
 
   function moscowClock() {
     let now = new Date().toLocaleString("en-US", {hour: '2-digit', minute:'2-digit', second:'2-digit', timeZone: "Europe/Moscow"});
+
+    let element = document.getElementById('moscow-clock');
+    let color = now.replace(/\D/g, '');
+    if (color.length < 6){
+      color = ('0'+ color);
+    }
+    color = color.match(/.{1,2}/g);
+
+    let hour = parseInt(color[0]);
+        hour += 65;
+        hour = hour.toString();
+    let minute = color[1];
+    let second = color[2];
+
+
+    // //appends 0 in front if hex number is single digit
+    if (hour.length < 2) {
+      hour = '0' + hour;
+    }
+    if (minute.length < 2) {
+      minute = '0' + minute;
+    }
+    if (second.length < 2) {
+      second = '0' + second;
+    }
+
+    element.style.backgroundColor = `#${hour}${minute}${second}`;
 
     $('#moscow-clock').text(`moscow ${now}`);
   }
